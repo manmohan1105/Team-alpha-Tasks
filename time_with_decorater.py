54295b16e6323db15ed2654813c523f76d9c2658
Manmohan Sharma <manmohan1105@gmail.com>
2022-06-21 22:43:18 +0530

    Add files via upload

---
+++ time_with_decorater.py
@@ -0,0 +1,31 @@
+'''
+Q Calculate the time taken by function using decorator in python
+
+'''
+from functools import wraps
+import time
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
+
+
+@timeit
+def calculate_something(num):
+    """
+    Simple function that returns sum of all numbers up to the square of num.
+    """
+    total = sum((x for x in range(0, num**2)))
+    return total
+
+if __name__ == '__main__':
+    calculate_something(100)
+  
