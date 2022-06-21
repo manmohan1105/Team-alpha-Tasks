d34e6151c9a51c704dae82207ac958fb5a0158f7
Manmohan Sharma <manmohan1105@gmail.com>
2022-06-21 23:25:41 +0530

    Add files via upload

---
+++ count_fun_call.py
@@ -0,0 +1,19 @@
+'''
+Q Calculate number of times function called using decorator in python
+'''
+def call_counter(func):
+    def helper(x):
+        helper.calls += 1
+        return func(x)
+    helper.calls = 0
+    return helper
+
+@call_counter
+def succ(x):
+    return x + 1
+
+if __name__ == '__main__':
+    print(succ.calls)
+    for i in range(10):
+        print(succ(i))
+    print(succ.calls)
